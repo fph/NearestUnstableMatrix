9d0a1ba2c8bb1995ec1f67ec9d2450a64cc2b0ce
Federico Poloni <federico.poloni@unipi.it>
2023-05-09 09:23:18 +0300

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,12 @@ using Random
 using LinearAlgebra
 
 @testset "NearestUnstableMatrix.jl" begin
-    # Write your tests here.
-    
+    A = [1. 2; 3 4]
+    v = [3/5;4/5]
+    w = [1;0.]
+    E = constrained_minimizer(A, v, w)
+    @test E ≈ [-0.72 -0.96; -3 -4]
+    @test constrained_optimal_value(A, v, w) ≈ norm(E)^2
     
     Random.seed!(0)
     n = 4
@@ -25,6 +29,7 @@ using LinearAlgebra
     @test (A+E)*v ≈ v*lambda
     @test abs(real(lambda)) < sqrt(eps(1.))
     @test abs(maximum(real(eigvals(A+E)))) < sqrt(eps(1.))
+    @test constrained_optimal_value(A, v) ≈ norm(E)^2
 
 end
 
