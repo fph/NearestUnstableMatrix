0bcfae93c233402b258afeb4ff026a78f9cbb293
Federico Poloni <federico.poloni@unipi.it>
2023-09-28 00:19:55 +0200

    fixed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,29 +18,33 @@ end
 @testset "NearestUnstableMatrix.jl" begin
     A = [1. 2; 3 4]
     v = [3/5;4/5]
-    w = [1;0.]
-    E = constrained_minimizer(A, v, w)
-    @test E ≈ [-0.72 -0.96; -3 -4]
-    @test constrained_optimal_value(A, v, w) ≈ norm(E)^2
+    target = Nonsingular
+    E, lambda = constrained_minimizer(A, v, target)
+    @test E ≈ [-1.32 -1.76; -3 -4]
+    @test lambda == 0
+    @test constrained_optimal_value(A, v, target) ≈ norm(E)^2
     
+    # Hurwitz
+
     Random.seed!(0)
     n = 4
-    A = rand(ComplexF64, (n,n))
+    A = rand(ComplexF64, (n,n)) - 2I
     v = normalize(rand(ComplexF64, n))
-    w = rand(ComplexF64, n)
-    E = constrained_minimizer(A, v, w)
-    @test (A+E)*v ≈ w
-    @test constrained_optimal_value(A, v, w) ≈ norm(E)^2
+    target = Hurwitz
+    E, lambda = constrained_minimizer(A, v, target)
+    @test (A+E)*v ≈ v*lambda
+    @test real(lambda) >= 0
+    @test constrained_optimal_value(A, v, target) ≈ norm(E)^2
 
-    # test LHP
+    # test sparse
 
     Random.seed!(0)
     A = Array(Tridiagonal(rand(ComplexF64,n-1), rand(ComplexF64,n), rand(ComplexF64,n-1)))
     A = A - maximum(real(eigvals(A))) * I - 0.1*I
 
-    E = constrained_minimizer(A, v, Hurwitz)
+    E, lambda = constrained_minimizer(A, v, Hurwitz)
     @test (E.==0) == (A.==0)
-    lambda = v'*(A+E)*v
+    @test lambda ≈ v'*(A+E)*v
     @test (A+E)*v ≈ v*lambda
     @test abs(real(lambda)) < sqrt(eps(1.))
     @test abs(maximum(real(eigvals(A+E)))) < sqrt(eps(1.))
@@ -52,9 +56,9 @@ end
     A = Array(Tridiagonal(rand(ComplexF64,n-1), rand(ComplexF64,n), rand(ComplexF64,n-1)))
     A = A * 0.9 /  maximum(abs.(eigvals(A)))
 
-    E = constrained_minimizer(A, v, Schur)
+    E, lambda = constrained_minimizer(A, v, Schur)
     @test (E.==0) == (A.==0)
-    lambda = v'*(A+E)*v
+    @test lambda ≈ v'*(A+E)*v
     @test (A+E)*v ≈ v*lambda
     @test abs(lambda) ≈ 1.
     @test maximum(abs.(eigvals(A+E))) ≈ 1.
@@ -64,11 +68,15 @@ end
 
     Random.seed!(0)
     n = 4
-    A = sprandn(ComplexF64, n,n, 0.5)
+    A = sprandn(ComplexF64, n,n, 0.5) / 2
     v = normalize(rand(ComplexF64, n))
-    w = rand(ComplexF64, n)
-    E = constrained_minimizer(A, v, w)
+    E, lambda = constrained_minimizer(A, v, Schur)
     @test (E.==0) == (A.==0)
-    @test (A+E)*v ≈ w
+    @test lambda ≈ v'*(A+E)*v
+    @test (A+E)*v ≈ v*lambda
+    @test abs(lambda) ≈ 1.
+    @test maximum(abs.(eigvals(Array(A+E)))) ≈ 1.
+    @test constrained_optimal_value(A, v, Schur) ≈ norm(E)^2
+
 end
 
